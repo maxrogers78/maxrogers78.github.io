3d6a392563a2f45220fde533db4d34392140222a
Max <maxrogerssepulveda@gmail.com>
2020-11-08 00:05:43 -0300

    projects info and navbar behavior added

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("breakingbad");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.backgroundColor = "#23252b";
+    document.getElementById("navbar").style.borderBottom = "5px solid #ffd131";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+    document.getElementById("navbar").style.borderBottom = "none";
   }
+  prevScrollpos = currentScrollPos;
 };
