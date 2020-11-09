2cfb307083d52ad57c3803380303dfbbf4b8b47c
Max <maxrogerssepulveda@gmail.com>
2020-11-08 21:00:51 -0300

    media queries added

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-    document.getElementById("navbar").style.backgroundColor = "#23252b";
-    document.getElementById("navbar").style.borderBottom = "5px solid #ffd131";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
-    document.getElementById("navbar").style.backgroundColor = "transparent";
-    document.getElementById("navbar").style.borderBottom = "none";
-  }
-  prevScrollpos = currentScrollPos;
-};
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+if (viewportWidth < 500) {
+} else if (viewportWidth < 700) {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+      document.getElementById("navbar").style.backgroundColor = "#23252b";
+      document.getElementById("navbar").style.borderBottom =
+        "2px solid #ffd131";
+    } else {
+      document.getElementById("navbar").style.top = "-100px";
+      document.getElementById("navbar").style.backgroundColor = "transparent";
+      document.getElementById("navbar").style.borderBottom = "none";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+} else {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+      document.getElementById("navbar").style.backgroundColor = "#23252b";
+      document.getElementById("navbar").style.borderBottom =
+        "5px solid #ffd131";
+    } else {
+      document.getElementById("navbar").style.top = "-100px";
+      document.getElementById("navbar").style.backgroundColor = "transparent";
+      document.getElementById("navbar").style.borderBottom = "none";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+}
